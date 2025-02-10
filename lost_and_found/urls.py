fe05405de90ef86f168c9a026293892e499fd51f
Samarth Sharma <samarths1703@gmail.com>
2025-02-10 20:42:58 +0530

    Added list view and template for displaying reported lost and found items

--- lost_and_found/urls.py
+++ lost_and_found/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, report_lost, report_found
+from .views import home, report_lost, report_found, items_list
 
 urlpatterns = [
     path('', home, name='home'),
     path('report-lost/', report_lost, name='report_lost'),
     path('report-found/', report_found, name='report_found'),
+    path('items/', items_list, name='items_list'),
 ]
--- lost_and_found/views.py
+++ lost_and_found/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import LostItemForm, FoundItemForm
+from .forms import LostItemForm, FoundItemForm, LostItem, FoundItem
 
 def home(request):
     return render(request, 'lost_and_found/home.html')  # we’ll create this next
@@ -23,3 +23,12 @@ def report_found(request):
     else:
         form = FoundItemForm()
     return render(request, 'lost_and_found/report_found.html', {'form': form})
+
+def items_list(request):
+    lost_items = LostItem.objects.all().order_by('-date_reported')
+    found_items = FoundItem.objects.all().order_by('-date_reported')
+    context = {
+        'lost_items': lost_items,
+        'found_items': found_items,
+    }
+    return render(request, 'lost_and_found/items_list.html', context)
