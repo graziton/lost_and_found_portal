d52d54d2c66191adc862dcb59a59bb881c91d8a7
Samarth Sharma <samarths1703@gmail.com>
2025-02-10 19:14:16 +0530

    Added LostItem and FoundItem models and registered them in admin

--- lost_and_found/admin.py
+++ lost_and_found/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import LostItem, FoundItem
 
-# Register your models here.
+admin.site.register(LostItem)
+admin.site.register(FoundItem)
--- lost_and_found/models.py
+++ lost_and_found/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class LostItem(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    location = models.CharField(max_length=100)
+    date_reported = models.DateField(auto_now_add=True)
+    image = models.ImageField(upload_to='lost_items/', null=True, blank=True)
+    status = models.CharField(max_length=20, default='lost')
+
+    def __str__(self):
+        return f"Lost: {self.title}"
+
+class FoundItem(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    location = models.CharField(max_length=100)
+    date_reported = models.DateField(auto_now_add=True)
+    image = models.ImageField(upload_to='found_items/', null=True, blank=True)
+    status = models.CharField(max_length=20, default='found')
+
+    def __str__(self):
+        return f"Found: {self.title}"
